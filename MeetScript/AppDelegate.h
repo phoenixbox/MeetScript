3abff671c043a727a1f1ad9791d711f06d6bc74a
Shane Rogers <rogerssh@tcd.ie>
2015-02-23 10:29:11 +0400

    project initialization

---
+++ MeetScript/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetScript
+//
+//  Created by Shane Rogers on 2/23/15.
+//  Copyright (c) 2015 SDR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ README.md
@@ -0,0 +1,3 @@
+## Meetscript
+
+iOS audio note transcriber. Audio note repository for meetings, helping with discovery and context review.
