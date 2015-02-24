fade5a31ddc0aceb8315fd209daf28c80a962dae
Shane Rogers <rogerssh@tcd.ie>
2015-02-24 08:18:17 +0400

    Add app constants

---
+++ MeetScript/AppConstants.h
@@ -0,0 +1,18 @@
+//
+//  AppConstants.h
+//  MeetScript
+//
+//  Created by Shane Rogers on 2/24/15.
+//  Copyright (c) 2015 SDR. All rights reserved.
+//
+
+#ifndef MeetScript_AppConstants_h
+#define MeetScript_AppConstants_h
+
+#define LINKEDIN_CLIENT_ID @"78pm6f9rexy8y0"
+#define LINKEDIN_CLIENT_SECRET @"I3UP1hetN5QavbPZ"
+// TODO: Define whether this is self generated or not
+#define LINKEDIN_STATE @"DCEEFWF45453sdffef424"
+#define LINKEDIN_ACCESS @[@"r_emailaddress",@"r_fullprofile",@" r_network",@"r_contactinfo"]
+
+#endif
